4030c6cb333a05f86850c58dd754875ac86922e1
Dang Nhz <41246555+dangnhz@users.noreply.github.com>
2020-07-22 19:39:36 +1000

    Updates

---
+++ precache-manifest.fff317808f8a1336ebf96554e744899a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c9aa5321590b95c83cd397b0127e6d",
+    "url": "/dangnhz/covid19-tracker/index.html"
+  },
+  {
+    "revision": "19342c83d65ed649b3c8",
+    "url": "/dangnhz/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "5496e4099c6dbc0fbe7d",
+    "url": "/dangnhz/covid19-tracker/static/css/main.dff9fbdc.chunk.css"
+  },
+  {
+    "revision": "19342c83d65ed649b3c8",
+    "url": "/dangnhz/covid19-tracker/static/js/2.44176749.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/dangnhz/covid19-tracker/static/js/2.44176749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5496e4099c6dbc0fbe7d",
+    "url": "/dangnhz/covid19-tracker/static/js/main.16f47127.chunk.js"
+  },
+  {
+    "revision": "ed2a01503d8a8e243819",
+    "url": "/dangnhz/covid19-tracker/static/js/runtime-main.776a8f50.js"
+  }
+]);
